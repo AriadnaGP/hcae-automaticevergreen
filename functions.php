da826022f958ae7314c7438745fd7f2b729bb929
Ariadnaj <ariadna.jugariu@gmail.com>
2024-03-26 18:24:22 +0200

    final edits

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file adds functions to the Frost WordPress theme.
  *
@@ -14,7 +15,7 @@ include_once get_template_directory() . '/includes/functions-blocks.php';
 include_once get_template_directory() . '/includes/functions-shortcodes.php';
 include_once get_template_directory() . '/includes/functions-general.php';
 
-if ( ! function_exists( 'frost_setup' ) ) {
+if (!function_exists('frost_setup')) {
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -27,10 +28,11 @@ if ( ! function_exists( 'frost_setup' ) ) {
 	 *
 	 * @return void
 	 */
-	function frost_setup() {
+	function frost_setup()
+	{
 
 		// Make theme available for translation.
-		load_theme_textdomain( 'frost', get_template_directory() . '/languages' );
+		load_theme_textdomain('frost', get_template_directory() . '/languages');
 
 		// Enqueue editor styles and fonts.
 		add_editor_style(
@@ -40,23 +42,24 @@ if ( ! function_exists( 'frost_setup' ) ) {
 		);
 
 		// Remove core block patterns.
-		remove_theme_support( 'core-block-patterns' );
+		remove_theme_support('core-block-patterns');
 	}
 }
-add_action( 'after_setup_theme', 'frost_setup' );
+add_action('after_setup_theme', 'frost_setup');
 
 // Enqueue style sheet.
-add_action( 'wp_enqueue_scripts', 'frost_enqueue_style_sheet' );
+add_action('wp_enqueue_scripts', 'frost_enqueue_style_sheet');
 
 /**
  * Implement styles and js.
  */
-function frost_enqueue_style_sheet() {
-	wp_enqueue_style( 'gpd-css', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' );
-  wp_enqueue_style( 'slick-carousel-theme', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css' );
-	wp_enqueue_script( 'gpd-js', get_template_directory_uri() . '/assets/js/custom.js', array( 'jquery' ), '1.8.1', true );
-	wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array( 'jquery' ), '1.8.1', true );
+function frost_enqueue_style_sheet()
+{
+	wp_enqueue_style('gpd-css', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+	wp_enqueue_style('slick-carousel-theme', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css');
+	wp_enqueue_script('gpd-js', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.8.1', true);
+	wp_enqueue_script('slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), '1.8.1', true);
 }
 
 /**
@@ -64,39 +67,45 @@ function frost_enqueue_style_sheet() {
  *
  * @since 0.9.2
  */
-function frost_register_block_styles() {
+function frost_register_block_styles()
+{
 
 	$block_styles = array(
 		'core/columns' => array(
-			'columns-reverse' => __( 'Reverse', 'frost' ),
+			'columns-reverse' => __('Reverse', 'frost'),
 		),
 		'core/group' => array(
-			'shadow-light' => __( 'Shadow', 'frost' ),
-			'shadow-solid' => __( 'Solid', 'frost' ),
+			'shadow-light' => __('Shadow', 'frost'),
+			'shadow-solid' => __('Solid', 'frost'),
 		),
 		'core/image' => array(
-			'shadow-light' => __( 'Shadow', 'frost' ),
-			'shadow-solid' => __( 'Solid', 'frost' ),
+			'shadow-light' => __('Shadow', 'frost'),
+			'shadow-solid' => __('Solid', 'frost'),
 		),
 		'core/list' => array(
-			'no-disc' => __( 'No Disc', 'frost' ),
-			'check-list' => __( 'Check', 'frost' ),
-			'numbered-list' => __( 'Numbered', 'frost' ),
+			'no-disc' => __('No Disc', 'frost'),
+			'check-list' => __('Check', 'frost'),
+			'numbered-list' => __('Numbered', 'frost'),
 		),
 		'core/navigation-link' => array(
-			'outline' => __( 'Outline', 'frost' ),
+			'outline' => __('Outline', 'frost'),
 		),
 		'core/quote' => array(
-			'shadow-light' => __( 'Shadow', 'frost' ),
-			'shadow-solid' => __( 'Solid', 'frost' ),
+			'shadow-light' => __('Shadow', 'frost'),
+			'shadow-solid' => __('Solid', 'frost'),
 		),
 		'core/social-links' => array(
-			'outline' => __( 'Outline', 'frost' ),
+			'outline' => __('Outline', 'frost'),
+		),
+		'core/heading' => array(
+			'green-wave-small' => __('Green Wave Small', 'frost'),
+			'green-wave-medium' => __('Green Wave Medium', 'frost'),
+			'green-wave-large' => __('Green Wave Large', 'frost'),
 		),
 	);
 
-	foreach ( $block_styles as $block => $styles ) {
-		foreach ( $styles as $style_name => $style_label ) {
+	foreach ($block_styles as $block => $styles) {
+		foreach ($styles as $style_name => $style_label) {
 			register_block_style(
 				$block,
 				array(
@@ -107,29 +116,30 @@ function frost_register_block_styles() {
 		}
 	}
 }
-add_action( 'init', 'frost_register_block_styles' );
+add_action('init', 'frost_register_block_styles');
 
 /**
  * Register block pattern categories.
  *
  * @since 1.0.4
  */
-function frost_register_block_pattern_categories() {
+function frost_register_block_pattern_categories()
+{
 
 	register_block_pattern_category(
 		'page',
 		array(
-			'label'       => __( 'Page', 'frost' ),
-			'description' => __( 'Create a full page with multiple patterns that are grouped together.', 'frost' ),
+			'label'       => __('Page', 'frost'),
+			'description' => __('Create a full page with multiple patterns that are grouped together.', 'frost'),
 		)
 	);
 	register_block_pattern_category(
 		'pricing',
 		array(
-			'label'       => __( 'Pricing', 'frost' ),
-			'description' => __( 'Compare features for your digital products or service plans.', 'frost' ),
+			'label'       => __('Pricing', 'frost'),
+			'description' => __('Compare features for your digital products or service plans.', 'frost'),
 		)
 	);
 }
 
-add_action( 'init', 'frost_register_block_pattern_categories' );
+add_action('init', 'frost_register_block_pattern_categories');
